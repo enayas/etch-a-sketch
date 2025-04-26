aed5e13cd5400c84fbdafb5880911637b988629f
Enaya Jawed <jawedenaya@gmail.com>
2025-04-25 21:45:37 -0500

    add grid through DOM and tweak css slightly

---
+++ script.js
@@ -0,0 +1,9 @@
+const container = document.querySelector(".container");
+
+for(let i=0; i<16; i++){
+  for(let j=0; j<16; j++){
+    const div = document.createElement("div");
+    div.classList.add("square");
+    container.appendChild(div);
+  }
+}
